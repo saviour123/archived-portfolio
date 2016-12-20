966686027f9ab4056125b5ec6cf82d0a0395ac1a
saviour gidi <profsaviour@gmail.com>
2016-12-20 15:41:44 +0000

    update

--- blog.py
+++ blog.py
@@ -4,6 +4,7 @@ import markdown
 import os
 from werkzeug import cached_property
 from flask_flatpages import FlatPages
+import yaml
 
 
 DEBUG = True
@@ -11,25 +12,41 @@ FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
 #app.config.from_object(__name__)
 
-
 app = Flask(__name__)
 app.secret_key = 'saviour123'
 #pages = FlatPages(app)
 POSTS_FILE_EXTENSION = '.md'
 
+#markdown rendering class
 class Post(object):
 	def __init__(self, path):
 		self.path = path
+		self._initialize_metadata()
 
 	@cached_property
 	def html(self):
 		with open(self.path, 'r') as fin:
-			content = fin.read().strip()
+			content = fin.read().split('\n', 1)[1].strip()
 		return markdown.markdown(content)  
-		
+	
+	def _initialize_metadata(self):
+			content = ''
+			with open(self.path, 'r') as fin:
+				for line in fin:
+					if not line.strip():
+						break
+					content += line
+			self.__dict__.update(yaml.load(content))
 
+@app.template_filter('date')
+def format_date(value, format='%B %d, %Y'):
+	return value.strftime(format)
 
+#app.jinja_env.filters['date'] = format_date
 
+# @app.context_processor
+# def inject_format_date():
+# 	return {'format_date': format_date}
 
 @app.route('/')
 def index():
@@ -39,9 +56,8 @@ def index():
 
 @app.route('/blog/<path:path>/')
 def post(path):	
-	import ipdb; ipdb.set_trace()
+	#import ipdb; ipdb.set_trace()
 	path = os.path.join('posts', path + POSTS_FILE_EXTENSION)
-	print path
 	post = Post(path)
 	return render_template('post.html', post=post)
 
--- posts/hello.md
+++ posts/hello.md
@@ -1,11 +1,7 @@
-title: hello
-#hello world
-***h3 level header***
+title: hello, world!
+date: 2016-03-24
 
-###h3 level header
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+#hello world, this is data from the meta data
+
+***h3 level header***
